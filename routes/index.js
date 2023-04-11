b0dd65fd71b6c39f70a19fdb9bc28d9df8efd767
Thomas <thomas.pierreleandre972@gmail.com>
2023-04-11 15:08:48 +0200

    mise en place backend

--- routes/index.js
+++ routes/index.js
@@ -6,23 +6,25 @@ const Trip = require('../models/trips')
 const { checkBody } = require('../modules/checkBody')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.post('/trips', (req, res) => {
-  if(checkBody(req.body, keys) === false){
-    res.json({result: false, message: "Empty fields, try again."})
+
+  if (!checkBody(req.body, ['departure', 'arrival', 'date'])) {
+    res.json({ result: false, message: "Empty fields, try again." })
     return;
   }
-  Trip.find({departure: req.body.departure, arrival: req.body.arrival, date: req.body.date})
-  .then(data => {
-    if (data !== null){
-    res.json({result: true, allTrips: data})}
-    else {
-      res.json({result: false, message: 'No trip found.'})
-    }
-  })
+  Trip.find({ departure: req.body.departure, arrival: req.body.arrival, date: req.body.date })
+    .then(data => {
+      if (data !== null) {
+        res.json({ result: true, allTrips: data })
+      }
+      else {
+        res.json({ result: false, message: 'No trip found.' })
+      }
+    })
 })
 
 
