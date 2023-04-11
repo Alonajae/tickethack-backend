93d1d0b15d33be2954acf8758a9c3f42c204ec80
Thomas <thomas.pierreleandre972@gmail.com>
2023-04-11 11:51:41 +0200

    mise en place backend

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const mongoose = require('mongoose');
+const Trip = require('../models/trips')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/trips', (req, res) => {
+  Trip.find({departure: req.body.departure, arrival: req.body.arrival, date: req.body.date})
+  .then(data => {
+    if (data !== null){
+    res.json({result: true, allTrips: data})}
+    else {
+      res.json({result: false, message: 'There is no trip available for this research'})
+    }
+  })
+})
 
 
 module.exports = router;
