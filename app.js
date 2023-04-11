45b06c4663407f115c7f35efc01a78978706db99
Thomas <thomas.pierreleandre972@gmail.com>
2023-04-11 10:26:58 +0200

    mise en place backend

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -17,7 +16,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 
 // This program generate trips in trips.json file. Feel free to modify or improve it!
