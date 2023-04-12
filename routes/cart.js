05d9f4634aa793780ceabe3d33049a9afebf4302
Thomas <thomas.pierreleandre972@gmail.com>
2023-04-12 14:31:34 +0200

    mise en place routes

--- routes/cart.js
+++ routes/cart.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-    
+const moment = require('moment');
+const mongoose = require('mongoose');
+const Cart= require('../models/carts');
+
+router.get('/', (req, res)=> {
+    Cart.find()
+    .then(data => {
+        res.json({result: true, allCarts: data})
+    })
+})
 
 module.exports = router;
